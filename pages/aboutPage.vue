3345b56812087b880265eebfbeddf66371c13d4f
mukeshsin <mukesh@innotechteam.in>
2023-08-10 16:34:36 +0530

    nuxt project

---
+++ pages/aboutPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <nav>
+      <NuxtLink to="/">Nuxt-Demo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+        <a href="/about">normal link -- about page</a>
+      </nav>
+    </header>
+
+    <h1>AboutPage</h1>
+    <p>this is my nuxt project</p>
+    <p>this is my nuxt project</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+     <p>products detail from {{id}}</p>
+     <p>this is my nuxt-demo i want to just learn this docs</p>
+    </div>
+</template>
+
+<script setup>
+ const {id} = useRoute().params 
+</script>
+
+<style scoped>
+
+</style>
